761d7f713667f31aab5e4cdfa05252b69f4b3d92
Ruairidh South <rsouth@princeton.edu>
2021-12-30 13:42:01 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.25

---
+++ src/UpwindDifferences.jl
@@ -0,0 +1,5 @@
+module UpwindDifferences
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using UpwindDifferences
+using Test
+
+@testset "UpwindDifferences.jl" begin
+    # Write your tests here.
+end
