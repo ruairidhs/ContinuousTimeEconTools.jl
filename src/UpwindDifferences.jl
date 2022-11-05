ed1eef2220e3a28187a1f4795ca2f0b1e54814f5
Ruairidh South <rsouth@princeton.edu>
2022-11-04 20:19:16 -0400

    Added simple backwards iteration functions

--- src/UpwindDifferences.jl
+++ src/UpwindDifferences.jl
@@ -5,7 +5,7 @@ Implements a finite differences method based on an upwind scheme to solve Hamilt
 """
 module UpwindDifferences
 
-import LinearAlgebra: diagind, Tridiagonal
+using LinearAlgebra
 
 #=
 Problem:
@@ -39,7 +39,7 @@ function policy_matrix!(A, UR::UpwindResult)
 end
 
 """
-    policy_matrix!(A, UR::UpwindResult)
+    policy_matrix(A, UR::UpwindResult)
 
 Return a tridiagonal matrix representation of the Poisson transition matrix implied by the drifts in `UR`.
 """
@@ -188,8 +188,44 @@ function get_interior_upwind(x, dvf, dvb, reward, drift, policy, zerodrift)
     return R, GF, GB
 end
 
+# ===== Backwards iteration functions =====
+struct ExplicitBackwardsIteration end
+struct ImplicitBackwardsIteration end
+
+"""
+    backwards_iterate!(v0, v1, r, A, ρ, Δ, method)
+
+Approximate `v0 ≡ v_t` given `v1 ≡ v_{t+Δ}` using the HJB equation: ``ρv = r + A * v + v̇``.
+
+# Methods
+
+Two methods are available:
+
+-  `ExplicitBackwardsIteration()` uses a backwards approximation of the time derivative, i.e.,
+   the method solves: ``ρv1 = r + A * v1 + (1 / Δ) * (v1 - v0)``;
+-  `ImplicitBackwardsIteration()` uses a forwards approximation of the time derivative, i.e,
+   the method solves: ``ρv0 = r + A * v0 + (1 / Δ) * (v1 - v0)``.
+"""
+function backwards_iterate!(v0, v1, r, A, ρ, Δ, ::ExplicitBackwardsIteration)
+    v0 .= r
+    mul!(v0, (ρ * I - A), v1, 1, -1)
+    # now v0 contains (1/Δ)(v1 - v)
+    v0 .*= -Δ
+    v0 .+= v1
+    return v0
+end
+
+function backwards_iterate!(v0, v1, r, A, ρ, Δ, ::ImplicitBackwardsIteration)
+    v0 .= r .+ (1 / Δ) .* v1
+    ldiv!(factorize((ρ + 1 / Δ) * I - A), v0)
+    return v0
+end
+
+# ===== Exports =====
 export UpwindResult, 
        upwind!, upwind,
-       policy_matrix, policy_matrix!
+       policy_matrix, policy_matrix!,
+       backwards_iterate!,
+       ImplicitBackwardsIteration, ExplicitBackwardsIteration
 
 end # module
